0a4ca2565c97089942557e9e46e34f42cf5d3e77
edwardfrary <edwardfrary@gmail.com>
2021-05-12 16:24:22 -0400

    finishing touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,7 +131,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -229,12 +229,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
+    $(this).addClass("dropover-active");
   },
   out: function (event) {
+    $(this).removeClass("dropover-active");
   },
   update: function (event) {
     //declare an array to store task data in
@@ -276,17 +282,26 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
+$(".bottom-trash").sortable()
 //date stuff starts here
 $("#modalDueDate").datepicker({ minDate: 0 });
+
+//sets a 30 min timer to run auditTask again
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
